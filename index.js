01da482d9cfaa238ef26256000b616fd5974b82d
vishal@0267 <vishaljat0267@gmail.com>
2022-04-26 18:20:36 +0530

    ok

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
 const express = require('express');
-const { default: mongoose } = require('mongoose');
+const mongoose= require('mongoose');
 const app = express();
-
-const DB = 'mongodb+srv://vishal:Vishal@0267@cluster0.c9vwu.mongodb.net/mernstackindexddd?retryWrites=true&w=majority'
-mongoose.connect((DB),{
+mongoose.connect( 'mongodb://localhost:27017/new',{
     useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnfiedTopology: true,
-    useFindAndModify: false
+    useUnifiedTopology: true,
+  
 }).then(()=>{
     console.log('connections successful');
 }).catch((err) => console.log('no connections'));
-
 
 
 
@@ -23,10 +19,10 @@ app.get('/',(req,res)=>{  // send database
     }]})
 })
 
-app.post('/username',(req,res)=>{ // new Entry 
-   console.log(">>>>>>>>>>>>>>>>>>>>>",req.body);
-   res.send({data: req.body, mas: "Data updated successfully"})
-})
+// app.post('/username',(req,res)=>{ // new Entry 
+//    console.log(">>>>>>>>>>>>>>>>>>>>>",req.body);
+//    res.send({data: req.body, mas: "Data updated successfully"})
+// })
 
 
 
